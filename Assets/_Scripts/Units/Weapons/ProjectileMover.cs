376e9b09be2b81168ac2c24acfcc676316cd43d7
intfloatbool <intfloatbool@gmail.com>
2021-04-14 16:48:47 +0300

    Bullet trail caching , Cinemachine package

--- Assets/_Scripts/Units/Weapons/ProjectileMover.cs
+++ Assets/_Scripts/Units/Weapons/ProjectileMover.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using _Scripts.Location;
+﻿using _Scripts.Location;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace _Scripts.Units.Weapons
 {
@@ -10,7 +8,13 @@ namespace _Scripts.Units.Weapons
         [SerializeField] private string _poolName;
         [SerializeField] private float _lifeTime = 5f;
         [SerializeField] private float _moveSpeed = 10f;
-        
+
+        [SerializeField] private TrailRenderer[] _trailRenderers;
+
+        private void OnValidate()
+        {
+            _trailRenderers = GetComponentsInChildren<TrailRenderer>(true);
+        }
 
         private void OnEnable()
         {
@@ -19,6 +23,13 @@ namespace _Scripts.Units.Weapons
 
         private void DisableProcess()
         {
+            foreach (var trail in _trailRenderers)
+            {
+                if (trail != null)
+                {
+                    trail.Clear();
+                }
+            }
             gameObject.SetActive(false);
             if (SceneGamePool.Instance != null)
             {
