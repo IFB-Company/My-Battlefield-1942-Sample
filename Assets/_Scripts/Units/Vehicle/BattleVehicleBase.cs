f493f60cecdf0ce55b0935a035a6af3f06f1f74a
intfloatbool <intfloatbool@gmail.com>
2021-04-15 17:05:21 +0300

    panzer moving

---
+++ Assets/_Scripts/Units/Vehicle/BattleVehicleBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using _Scripts.Units.Base;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace _Scripts.Units.Vehicle
+{
+    public abstract class BattleVehicleBase : BattleUnitBase
+    {
+        protected FootmanBattleUnit _pilot;
+        public FootmanBattleUnit Pilot => _pilot;
+
+        public bool IsOnControl  => _pilot != null;
+
+        public event Action<BattleVehicleBase, FootmanBattleUnit> OnEnterInVehicle; 
+        public event Action<BattleVehicleBase, FootmanBattleUnit> OnLeaveFromVehicle; 
+        
+        public virtual void EnterVehicle(FootmanBattleUnit footmanBattleUnit)
+        {
+            if (IsOnControl)
+            {
+                Debug.LogError($"Cannot Enter vehicle it is already on control!");
+                return;
+            }
+            Assert.IsNotNull(footmanBattleUnit, "footmanBattleUnit != null");
+            _pilot = footmanBattleUnit;
+            
+            OnEnterInVehicle?.Invoke(this, _pilot);
+        }
+
+        public virtual void LeaveVehicle()
+        {
+            if (!IsOnControl)
+            {
+                Debug.LogError($"LEave from vehicle that is'nt on control!");
+                return;
+            }
+            
+            OnLeaveFromVehicle?.Invoke(this, _pilot);
+            _pilot = null;
+        }
+    }
+}
---
+++ Assets/_Scripts/Units/Vehicle/PanzerVehicle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Units.Vehicle
+{
+    public class PanzerVehicle : BattleVehicleBase
+    {
+        [SerializeField] private float _torque = 200f;
+        [SerializeField] private Rigidbody _leadRb;
+        [SerializeField] private ConstantForce _constantForce;
+        protected virtual void OnValidate()
+        {
+            _leadRb = GetComponent<Rigidbody>();
+            _constantForce = GetComponent<ConstantForce>();
+        }
+        
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public override void MoveAtFrame(Vector3 dir)
+        {
+            _constantForce.force = Vector3.forward * dir.z * _torque;
+        }
+
+        public override void RotateAtFrame(Vector3 dir)
+        {
+            
+        }
+    }
+}
