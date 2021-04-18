2b1dc64cf348ebbeafa191bffa48463cb174cb32
intfloatbool <intfloatbool@gmail.com>
2021-04-18 11:39:46 +0300

    Die controller base

---
+++ Assets/_Scripts/Units/DieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+namespace _Scripts.Units
+{
+    public class DieController : MonoBehaviour
+    {
+        [SerializeField] private HittableObject _hittableObject;
+        [SerializeField] private UnityEvent _onDieUnityEvent;
+        
+        private void Awake()
+        {
+            Assert.IsNotNull(_hittableObject, "_hittableObject != null");
+            _hittableObject.OnDie += HittableObjectOnDie;
+        }
+
+        private void OnDestroy()
+        {
+            _hittableObject.OnDie -= HittableObjectOnDie;
+        }
+
+        private void HittableObjectOnDie()
+        {
+            //TODO: Realize real die
+            _onDieUnityEvent?.Invoke();
+        }
+    }
+}
