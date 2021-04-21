e094c9398b357a53d5c3e97fce2997b13af8d86d
intfloatbool <intfloatbool@gmail.com>
2021-04-21 17:18:02 +0300

    Scene reset and fixes

---
+++ Assets/_Scripts/UI/DebugTesting/RestartCurrentSceneBtn.cs
@@ -0,0 +1,17 @@
+﻿using Common.UI;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts.UI.DebugTesting
+{
+    public class RestartCurrentSceneBtn : ButtonBase
+    {
+        protected override void OnClick()
+        {
+            var currentScene = SceneManager.GetActiveScene();
+            if (currentScene != null)
+            {
+                SceneManager.LoadScene(currentScene.name);
+            }
+        }
+    }
+}
