8812d74fa8de0873deef9c4338eed61d738a32ac
intfloatbool <intfloatbool@gmail.com>
2021-04-21 16:15:09 +0300

    Fix bombs

---
+++ Assets/_Scripts/Units/OnDisableEffectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using _Scripts.Location;
+using UnityEngine;
+
+namespace _Scripts.Units
+{
+    public class OnDisableEffectSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _effectPrefab;
+        private void OnDisable()
+        {
+            if (_effectPrefab != null)
+            {
+                var sceneGamePool = SceneGamePool.Instance;
+                GameObject effectInstance = null;
+                if (sceneGamePool != null)
+                {
+                    effectInstance = sceneGamePool.GetObjectFromPool(_effectPrefab);
+                }
+                else
+                {
+                    effectInstance = Instantiate(_effectPrefab);
+                }
+
+                effectInstance.transform.position = transform.position;
+                effectInstance.transform.rotation = transform.rotation;
+            }
+        }
+    }
+}
